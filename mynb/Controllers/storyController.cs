aa73fbf3ee7335dc0ffe2127736070287904f1e8
Alex <aleks.efy@gmail.com>
2017-03-23 17:14:44 +0300

    Добавление  try/catch в MySQL.cs + проверка подключения в storyController.cs

--- mynb/Controllers/storyController.cs
+++ mynb/Controllers/storyController.cs
@@ -9,30 +9,51 @@ namespace mynb.Controllers
 {
     public class storyController : Controller
     {
+        public ActionResult ErrorActionResult;
         // GET: story
         public ActionResult Index()
         {
-            return View(); // "random"
+            Story story = new Story();
+            if (IsError()) return ErrorActionResult;
+
+            return View("number", story);
+
         }
 
 
         public ActionResult random()
         {
             Story story = new Story();
+            if (IsError()) return ErrorActionResult;
             story.Random();
             return View("number", story);
         }
 
         public ActionResult add()
         {
+            if (IsError()) return ErrorActionResult;
             return View();
         }
 
         // показывает одну историю/уже две
         public ActionResult number()
         {
+            if (IsError()) return ErrorActionResult;
             Story story = new Story();
             return View(story);
         }
+
+        public bool IsError()
+        {
+            if (MySQL.IsError())
+            {
+                ViewBag.error = MySQL.error;
+                ViewBag.query = MySQL.query;
+                ErrorActionResult = View("~/Views/Error.cshtml");
+                return true;
+            }
+            return false;
+        }
+
     }
 }
--- mynb/Models/MySQL.cs
+++ mynb/Models/MySQL.cs
@@ -11,23 +11,49 @@ namespace mynb.Models
     static public class MySQL
     {
         static private MySqlConnection con;
+        static public string error { get; private set; }
+        static public string query { get; private set; }
 
         static MySQL()
         {   // строка подключения к БД
             // con = new MySqlConnection("server=localhost; UserID=root;Password=mysql;database=STORY;Charset=utf8");
             //создаем подключение
-            con = new MySqlConnection(
-                WebConfigurationManager.ConnectionStrings["conn"].ConnectionString);
-            con.Open();
+            try
+            {
+                error = "";
+                query = 
+                con = new MySqlConnection(
+                    WebConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+                con.Open();
+            } catch (Exception ex)
+            {
+                error = ex.Message;
+                query = "OPEN CONNECTION TO MySQL";
+                con = null;
+            }
+
+            
         }
         static public DataTable Select(string myquery)
         {
-            DataTable table = new DataTable();
-            MySqlCommand cmd = new MySqlCommand(myquery, con);
-            MySqlDataReader reader = cmd.ExecuteReader();
-            table.Load(reader);
-            return table;
+            if (IsError()) return null;
+            try
+            {
+                DataTable table = new DataTable();
+                MySqlCommand cmd = new MySqlCommand(myquery, con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                table.Load(reader);
+                return table;
+            }
+            catch
+            { }
         }
+        static public bool IsError()
+        {
+            
+            return error != "";
+        }
+
 
     }
 }
